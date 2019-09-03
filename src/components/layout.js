5c052167135680c0d75d3bf11ee9157a7078b9be
rgcavalcanti <rcavalcanti.ti@gmail.com>
2019-09-02 23:48:43 -0300

    Working on index page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,27 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { Link } from "gatsby"
+import styled from "styled-components"
 
-import Header from "./header"
-import "./layout.css"
+const Header = styled.header``
+
+const Content = styled.main`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+`
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Header>
+        {/* <nav>
+          <Link to="/">Início</Link>
+          <Link to="/about">Sobre</Link>
+        </nav> */}
+      </Header>
+      <Content>{children}</Content>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,26 @@
 import React from "react"
 
-export default () => <h1>Hello World!</h1>
+import Layout from "../components/layout"
+import avatar from "../images/avatar.jpg"
+import styled from "styled-components"
+
+const Avatar = styled.div`
+  width: 200px;
+  height: 200px;
+  margin-top: 3rem;
+  border-radius: 100%;
+  background-size: cover;
+  background-image: url(${avatar});
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
+`
+const Info = styled.h1`
+  font-size: 1.6rem;
+`
+
+export default () => (
+  <Layout>
+    <Avatar src={avatar} />
+    <Info>Rafael G. Cavalcanti</Info>
+    <p>Desenvolvedor Full-Stack.</p>
+  </Layout>
+)
