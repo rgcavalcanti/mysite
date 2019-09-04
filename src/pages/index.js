ee201d4325a9b57c44ba42c0a5986ec551caa6dd
rgcavalcanti <rcavalcanti.ti@gmail.com>
2019-09-03 23:07:40 -0300

    Init styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,20 +7,29 @@ import styled from "styled-components"
 const Avatar = styled.div`
   width: 200px;
   height: 200px;
-  margin-top: 3rem;
   border-radius: 100%;
   background-size: cover;
   background-image: url(${avatar});
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
 `
-const Info = styled.h1`
+const Title = styled.h1`
+  font-size: 2.4rem;
+  color: #3299bb;
+`
+
+const Subtitle = styled.p`
   font-size: 1.6rem;
+  text-align: center;
+  color: #424242;
 `
 
 export default () => (
   <Layout>
     <Avatar src={avatar} />
-    <Info>Rafael G. Cavalcanti</Info>
-    <p>Desenvolvedor Full-Stack.</p>
+    <Title>Rafael G. Cavalcanti</Title>
+    <Subtitle>
+      Desenvolvedor Full-Stack. Apaixonado por novas tecnologias e amante de
+      novos desafios
+    </Subtitle>
   </Layout>
 )
